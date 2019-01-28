807ce85423367bf5d60e85ad0200e98f16e5472d
Stefanos <sagelastos@gmail.com>
2019-01-28 19:15:31 +0100

    one can select a main menu option, only the project displays something

--- sketch.js
+++ sketch.js
@@ -5,34 +5,98 @@
  * @date 2017/02/04
  */
 
+var colorPalete;
 var colors;
+var options = [];
 var type;
 var waveWidth = 100;
+var XMainMenu;
+var waveCenter;
+var mainMenuYPositions;
 
 function setup() {
-  colors = [
+
+  /* -- SETUP FIELDS -- */
+  colorPalete = [
     color(255, 0, 0),
     color(0, 255, 0),
     color(0, 0, 255)
   ];
+
+  colors = colorPalete.slice(0);
+
+  waveCenter = windowHeight / 2 - 35;
+
+  XMainMenu = windowWidth / 20;
+
+  mainMenuYPositions = [
+    waveCenter - waveWidth - 3 * waveWidth / 4,
+    waveCenter - waveWidth - 2 * waveWidth / 4,
+    waveCenter - waveWidth - waveWidth / 4,
+    waveCenter,
+    waveCenter + waveWidth + waveWidth / 4,
+    waveCenter + waveWidth + 2 * waveWidth / 4,
+    waveCenter + waveWidth + 3 * waveWidth / 4
+  ];
+
+  /* -------- */
+  /* -- CREATE CANVAS -- */
   createCanvas(windowWidth, windowHeight);
-  var portfolioP = createP("STEFWORKS");
-  portfolioP.position(width / 4, height / 2 - 35 ).style('font-size', '20px');
-  var projects = createP("My projects");
-  projects.position(width / 4, height / 2 - 25 + waveWidth/4).style('color', colors[0]).mousePressed(() =>{
-    select("#Demo3").position(0,windowHeight-100).show();
-    select("#Content3").position(windowWidth/2-select("#Content3").size().width/2,0).style("z-index","10");
+  /* -------- */
+
+  /* --- CREATE MAIN MENU OPTIONS AND SET INITIAL POSITIONS --- */
+  var _rootMenu = createP("STEFWORKS");
+  _rootMenu.position(XMainMenu, mainMenuYPositions[3]).style('font-size', '20px');
+  options.push(_rootMenu);
+
+  var _projects = createP("My projects");
+  _projects.position(XMainMenu, mainMenuYPositions[4]).style('color', colors[0]).style('font-size', '20px');
+  options.push(_projects);
+
+  var _selfies = createP("My life in selfies");
+  _selfies.position(XMainMenu, mainMenuYPositions[5]).style('color', colors[1]).style('font-size', '20px');
+  options.push(_selfies);
+
+  var _experience = createP("My professional experience");
+  _experience.position(XMainMenu, mainMenuYPositions[6]).style('color', colors[2]).style('font-size', '20px');
+  options.push(_experience);
+
+  /* --------- */
+
+  /* --- SETUP SCROLLING FUNCTIONALITY --- */
+
+  /* ---------- */
+
+  /* --- SETUP ONCLICK METHODS */
+
+  _rootMenu.mousePressed(() => {mainMenuSelect(_rootMenu)});
+  _projects.mousePressed(() => {
+    select("#Demo3").position(0, windowHeight - 100).show();
+    select("#Content3").position(windowWidth / 2 - select("#Content3").size().width / 2, 0).style("z-index", "10");
     select(".Section3").show();
-    colors.shift();
+    mainMenuSelect(_projects);
   });
-  var selfies = createP("My life in selfies");
-  selfies.position(width / 4, height / 2 - 25 + 2*waveWidth/4).style('color', colors[1]);
-  var experience = createP("My professional experience");
-  experience.position(width / 4, height / 2 - 25 + 3*waveWidth/4).style('color', colors[2]);
-  portfolioP.mousePressed({});
+  _selfies.mousePressed(() => {mainMenuSelect(_selfies)});
+  _experience.mousePressed(() => {mainMenuSelect(_experience)});
 
+  /* --------- */
 
+  /* -- SETUP BLEND TYPE FOR THE WAVE ~ 0 is black, 1 is white -- */
   type = 0;
+}
+
+/* method that shifts the option positions so that the selected is in position 3 */
+function mainMenuSelect(selected) {
+  var selectedIndex = options.indexOf(selected);
+  console.log("selected: "+selectedIndex);
+  var i;
+  for(i = 0; i < options.length; i++){
+    options[i].position(XMainMenu, mainMenuYPositions[3 - selectedIndex + i]);
+  }
+  colors = colorPalete.slice(0);
+  if(selectedIndex != 0){
+    colors.splice( selectedIndex-1 ,1);
+  }
 }
 
 
@@ -61,13 +125,11 @@ function draw() {
     }
     endShape();
   });
+  /* -- TRANSLATE OPTIONS TO RIGHT POSITION */
   
-}
-
-function moveElement(element, end) {
-
 
 }
+
 
 /* function mousePressed() {
   colors.pop();
